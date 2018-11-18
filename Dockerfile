180449f0bfc30f3a70259d63782f33a2b108171c
Koh Meng Wang <mengwangk@gmail.com>
2018-11-18 22:56:01 +0800

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3
+FROM python:latest
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /code
 WORKDIR /code
--- manage.py
+++ manage.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
-import os
-import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangosite.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
